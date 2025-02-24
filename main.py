40f811b98483765279fbe29c75ce9152f7f41b81
astrobyte <robert@swansontech.net>
2025-02-23 21:20:53 -0500

    sunday work

--- main.py
+++ main.py
@@ -2,9 +2,17 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
